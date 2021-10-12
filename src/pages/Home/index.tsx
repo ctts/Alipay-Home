588766c31f2d7380ea0c9c9c36240ad87d4514af
taojiahang <taojiahang@gmail.com>
2021-10-13 07:53:56 +0800

    feat: home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,27 @@
 import styles from './index.less';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Footer from './components/Foot';
+import Money from './components/Money';
+import DataBoard from './components/DataBoard';
+import { Layout } from 'antd';
+
+const { Sider, Content } = Layout;
 
 export default function IndexPage() {
   return (
     <div className={styles.container}>
-      <Navbar/>
+      <div>
+        <Navbar />
+        <div className={styles.main}>
+          <div>
+            <DataBoard />
+          </div>
+          <div className={styles.rightPanel}>
+            <Money />
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
