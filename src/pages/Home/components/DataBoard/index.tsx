ca3dc5ea6a622c6e64dd800976c177c6bd344aba
taojiahang <taojiahang@gmail.com>
2021-10-13 07:51:43 +0800

    feat databoard

---
+++ src/pages/Home/components/DataBoard/index.tsx
@@ -0,0 +1,21 @@
+import style from './index.less';
+import ECharts from '@/pages/components/ECharts';
+import { Tabs } from 'antd';
+const { TabPane } = Tabs;
+export default function Navbar() {
+  return (
+    <div className={style.dataBoard}>
+      {/* <div>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="Tab 1" key="1" style={{ height: 200 }}>
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+          </TabPane>
+        </Tabs>
+      </div> */}
+      <ECharts />
+    </div>
+  );
+}
