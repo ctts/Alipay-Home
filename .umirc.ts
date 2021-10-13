30691b70aae4e71fb3a82b38e758a50600294da4
taojiahang <taojiahang@gmail.com>
2021-10-13 10:35:38 +0800

    feat: add dist

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  base: '/docs/',
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
 });
