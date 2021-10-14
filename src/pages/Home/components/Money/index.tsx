1c2b430a3862b091f96dc25575ed9aee95ce15c4
taojiahang <taojiahang@gmail.com>
2021-10-14 22:57:36 +0800

    feat: 余额添加渐变;

--- src/pages/Home/components/Money/index.tsx
+++ src/pages/Home/components/Money/index.tsx
@@ -26,38 +26,36 @@ export default function Money() {
     </div>
   );
   return (
-    <div>
-      <div className={styles.money}>
-        <div className={styles.flexBetween}>
-          <div className={styles.title}>
-            <div className={styles.myMoney}>我的余额</div>
-            {showDetail ? (
-              <EyeOutlined onClick={showStateChange} />
-            ) : (
-              <EyeInvisibleOutlined onClick={showStateChange} />
-            )}
-          </div>
-          <div className={styles.rightArrow}>资金管理</div>
+    <div className={styles.money}>
+      <div className={styles.flexBetween}>
+        <div className={styles.title}>
+          <div className={styles.myMoney}>我的余额</div>
+          {showDetail ? (
+            <EyeOutlined onClick={showStateChange} />
+          ) : (
+            <EyeInvisibleOutlined onClick={showStateChange} />
+          )}
         </div>
-        <div className={styles.divider}></div>
-        <div>
-          <Row gutter={16}>
-            <Col span={8}>
-              <div className={styles.moneyTitle}>账户可用余额（元）</div>
-              {showDetail ? <Statistic value={112893} /> : HideItem}
-            </Col>
-            <Col span={7}></Col>
-            <Col span={8}>
-              {question}
-              {showDetail ? <Statistic value={112893} /> : HideItem}
-            </Col>
-          </Row>
-          <div className={styles.mt38}>
-            <Button className={styles.mr6}>充值</Button>
-            <Button className={styles.mr6}>转账</Button>
-            <Button className={styles.mr6}>提现</Button>
-            <Button>批量付款</Button>
-          </div>
+        <div className={styles.rightArrow}>资金管理</div>
+      </div>
+      <div className={styles.divider}></div>
+      <div>
+        <Row gutter={16}>
+          <Col span={8}>
+            <div className={styles.moneyTitle}>账户可用余额（元）</div>
+            {showDetail ? <Statistic value={112893} /> : HideItem}
+          </Col>
+          <Col span={7}></Col>
+          <Col span={8}>
+            {question}
+            {showDetail ? <Statistic value={112893} /> : HideItem}
+          </Col>
+        </Row>
+        <div className={styles.mt38}>
+          <Button className={styles.mr6}>充值</Button>
+          <Button className={styles.mr6}>转账</Button>
+          <Button className={styles.mr6}>提现</Button>
+          <Button>批量付款</Button>
         </div>
       </div>
     </div>
