fa4a3eda7bc89d30ea620c1de1e613a63db89bcc
taojiahang <taojiahang@gmail.com>
2021-10-14 08:53:03 +0800

    feat: 导航栏样式优化

--- src/pages/Home/components/Navbar/index.tsx
+++ src/pages/Home/components/Navbar/index.tsx
@@ -1,8 +1,6 @@
 import styles from './index.less';
 import { SoundOutlined, QuestionCircleOutlined } from '@ant-design/icons';
-import { Layout } from 'antd';
-
-const { Header } = Layout;
+import { Dropdown, Menu } from 'antd';
 
 const navList = [
   { name: '首页', href: '/' },
@@ -15,8 +13,15 @@ const navList = [
 ];
 
 export default function Navbar() {
+  const menu = (
+    <Menu className={styles.userMenu}>
+      <Menu.Item>返回个人版</Menu.Item>
+      <Menu.Item>切换账号</Menu.Item>
+      <Menu.Item>退出登录</Menu.Item>
+    </Menu>
+  );
   return (
-    <Header className={styles.navbar}>
+    <div className={styles.navbar}>
       <div>
         <img
           className={styles.logo}
@@ -32,10 +37,23 @@ export default function Navbar() {
           </a>
         ))}
       </nav>
-      <div className={styles.user}>
-        <SoundOutlined />
-        <QuestionCircleOutlined />
+      <div className={styles.userWrapper}>
+        <SoundOutlined className={styles.userIcon} />
+        <QuestionCircleOutlined className={styles.userIcon} />
+        <Dropdown overlay={menu} placement="bottomCenter" arrow={false}>
+          <div className={styles.info}>
+            <img
+              src="https://gw.alipayobjects.com/mdn/rms_50301b/afts/img/A*KspsSIavJjgAAAAAAAAAAAAAARQnAQ"
+              alt="logo"
+              className={styles.avatar}
+            ></img>
+            <div className={styles.user}>
+              <div>陶家行</div>
+              <div className={styles.account}>178******23 [主账号]</div>
+            </div>
+          </div>
+        </Dropdown>
       </div>
-    </Header>
+    </div>
   );
 }
