a92668159bb222366371b6440013a4652632182e
taojiahang <taojiahang@gmail.com>
2021-10-14 19:14:12 +0800

    feat: 样式优化

--- src/pages/Home/components/Foot/index.tsx
+++ src/pages/Home/components/Foot/index.tsx
@@ -1,13 +1,9 @@
 import styles from './index.less';
 
-import { Layout } from 'antd';
-
-const { Footer } = Layout;
-
 export default function Foot() {
   return (
-    <Footer className={styles.footer}>
+    <div className={styles.footer}>
       经营许可证编号：合字B2-20190046 网站备案：沪ICP备 15027489号
-    </Footer>
+    </div>
   );
 }
