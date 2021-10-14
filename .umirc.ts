835b559527565d5ad1a82c219919053930a67185
taojiahang <taojiahang@gmail.com>
2021-10-14 08:11:49 +0800

    feat: 添加favicon title

--- .umirc.ts
+++ .umirc.ts
@@ -7,4 +7,6 @@ export default defineConfig({
   },
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  title: '支付宝商家中心-国内领先的第三方支付和金融服务平台',
+  favicon: '/assets/favicon.ico',
 });
